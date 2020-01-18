e0935db12706cb5ccba095c744a20f54b63b8947
hanMaster <w54661c@gmail.com>
2020-01-19 00:27:33 +1000

    STP

---
+++ stp/precache-manifest.687932fdc6fb64337fda6404088acc65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08ea681d5678e819c131531058fab17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56673ecc268218653201",
+    "url": "/static/css/main.984c0aee.chunk.css"
+  },
+  {
+    "revision": "0e3afe058291db49e688",
+    "url": "/static/js/2.ae60a524.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.ae60a524.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d396c76846297855a0b",
+    "url": "/static/js/3.85e8c855.chunk.js"
+  },
+  {
+    "revision": "b917215da125b3c2677865e81b0c0b55",
+    "url": "/static/js/3.85e8c855.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56673ecc268218653201",
+    "url": "/static/js/main.1b2ed5e0.chunk.js"
+  },
+  {
+    "revision": "4f4303371310a097c6b6",
+    "url": "/static/js/runtime-main.8aec10c7.js"
+  },
+  {
+    "revision": "2d4a94ab174085732c0959970aeee5fb",
+    "url": "/static/media/check.2d4a94ab.svg"
+  },
+  {
+    "revision": "7d6e764651c770576d72711aaa9a54fe",
+    "url": "/static/media/map.7d6e7646.png"
+  }
+]);
